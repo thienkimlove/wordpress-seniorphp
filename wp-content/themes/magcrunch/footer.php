8b62cc13e8f733a19f23b3c0a9d31ed3ffb16369
Tieu Ngao <thienkimlove@gmail.com>
2016-04-25 00:08:34 -0400

    test

--- wp-content/themes/magcrunch/footer.php
+++ wp-content/themes/magcrunch/footer.php
@@ -37,6 +37,24 @@
 	</footer><!-- #colophon -->
 
 <?php wp_footer(); ?>
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
 
+fbq('init', '1312502752098780');
+fbq('track', "PageView");</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1312502752098780&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+<script>
+// ViewContent
+// Track key page views (ex: product page, landing page or article)
+fbq('track', 'ViewContent');
+</script>
 </body>
 </html>
